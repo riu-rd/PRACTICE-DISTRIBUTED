14adbf043690e14ca0230688126fea047c8b38da
Darius <darius.vincent5@gmail.com>
2025-04-11 01:48:09 +0800

    Sync Code

--- Assignment 3/sync-code/05-dining-philosophers/dining-philosophers.cpp
+++ Assignment 3/sync-code/05-dining-philosophers/dining-philosophers.cpp
@@ -8,6 +8,7 @@
 #include <mutex>
 #include <semaphore>
 #include <chrono>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,6 +16,7 @@ using namespace std;
 
 int chopstick_Status[5] = {0, 0, 0, 0, 0};
 int philosopher_Status[5] = {0, 0, 0, 0, 0};
+counting_semaphore print_m(1);
 
 void philosophize(int id)
 {
@@ -23,12 +25,15 @@ void philosophize(int id)
     // Pick the two chopsticks
     chopstick_Status[(id + 0) % 5] = 1; // left chopstick at index id
     chopstick_Status[(id + 1) % 5] = 1; // right chopstick at index id+1
+
+    print_m.acquire();
     string s = to_string(chopstick_Status[0]);
     s += to_string(chopstick_Status[1]);
     s += to_string(chopstick_Status[2]);
     s += to_string(chopstick_Status[3]);
     s += to_string(chopstick_Status[4]);
     cout << "P" << id << ": " << s << "\n";
+    print_m.release();
 
     // Philosopher 'Eating'
     philosopher_Status[id] = 1;
@@ -50,11 +55,13 @@ counting_semaphore<1> d(1);
 counting_semaphore<1> e(1);
 counting_semaphore<1> *s_c[5] = {&a, &b, &c, &d, &e};
 counting_semaphore m(1);
+counting_semaphore<2> max_philosophers(2);
 
 void _philosophize(int id)
 {
   for (int i = 0; i < 5; i++)
   {
+    max_philosophers.acquire();
     // Wait for the two chopsticks to be available
     s_c[id]->acquire();
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -86,6 +93,8 @@ void _philosophize(int id)
     // Release the two chopsticks / Signal that the two chopsticks are free
     s_c[id]->release();
     s_c[(id + 1) % 5]->release();
+
+    max_philosophers.release();
   }
 }
 
